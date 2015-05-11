de1557459591c0ea8e91afac2c81e1ab32af40f3
DevSusu <os1742@gmail.com>
2015-05-11 10:49:57 +0900

    Check file content types

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 require 'sinatra'
 
+preffered_content_types = ['audio/wav' , 'application/octet-stream']
+
 get '/' do
  'hello world!'
 end
 
 post '/records' do
   p params
+
+  if not preffered_content_types.include? params['file'][:type]
+    status 415 # Unsupported Media Type
+    return "Unsupported Media Type. Only support #{preffered_content_types}"
+  end
+
   File.open('records/' + params['file'][:filename], "w") do |f|
     f.write(params['file'][:tempfile].read)
   end
