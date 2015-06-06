1670b0744ee4f80976707aa22639952c7ac6a080
DevSusu <os1742@gmail.com>
2015-06-07 03:41:56 +0900

    upload, nr, remove, async.. all done

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'fog'
 require 'dotenv'
 Dotenv.load
-set :port, 80
+#set :port, 80
 
 preffered_content_types = ['application/octet-stream' , 'audio/ogg']
 
@@ -30,14 +30,16 @@ get '/nr' do
       `rm #{uuid}.ogg`
 
       # Upload NR file
-      if upload(uuid)
+      if upload(uuid+'-nr.ogg')
         # upload successful
         # Send API request to inform server
         reducted(uuid)
       end
+
+      `rm #{uuid}-nr.ogg`
     end
 
-    'https://parrote.s3.amazonaws.com/uploads/#{uuid}-nr.ogg'
+    'https://parrote.s3.amazonaws.com/uploads/%s-nr.ogg' % uuid
 
   else
     'URL is not in an appropriate format'
@@ -49,10 +51,10 @@ def reducted(uuid)
   # Create a request on rails server
   # find record with uuid
   # change record's status to Reducted
-  uri = URI.parse("http://www.loroclip.com/api/v1/records/nr")
+  uri = URI.parse("http://localhost:3000/api/v1/records/nr")
   params = {
     :url => 'https://parrote.s3.amazonaws.com/uploads/#{uuid}-nr.ogg',
-    :uuid => uuid
+    :uuid => uuid,
   }
   res = Net::HTTP.post_form(uri, params)
 
@@ -69,8 +71,8 @@ def upload(filename)
   directory = connection.directories.get(ENV['AWS_BUCKET_NAME']);
 
   file = directory.files.new({
-    :key                    => ENV['AWS_BUCKET_NAME'],
-    :body                   => File.open(uuid+'-nr.ogg'),
+    :key                    => 'uploads/' + filename,
+    :body                   => File.open(filename),
     :public                 => true,
     })
 
