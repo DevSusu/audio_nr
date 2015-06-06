2ec15c06f098f1bf4e6b4bcaee5ea4ad8627e39f
DevSusu <os1742@gmail.com>
2015-06-06 14:03:51 -0400

    added request nr flag after reduction

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'fog'
 require 'dotenv'
 Dotenv.load
-#set :port, 80
+set :port, 80
 
 preffered_content_types = ['application/octet-stream' , 'audio/ogg']
 
@@ -23,11 +23,11 @@ get '/nr' do
 
     pid = fork do
       # Download recording from s3
-      # `curl -o #{uuid}.ogg #{url}`
+      `curl -o #{uuid}.ogg #{url}`
       # NR on white noise
-      # `sox #{uuid}.ogg #{uuid}-nr.ogg noisered noise_white.noise-profile 0.2`
+      `sox #{uuid}.ogg #{uuid}-nr.ogg noisered noise_white.noise-profile 0.2`
       # Clean up
-      # `rm #{uuid}.ogg`
+      `rm #{uuid}.ogg`
 
       # Upload NR file
       if upload(uuid)
@@ -37,24 +37,10 @@ get '/nr' do
       end
     end
 
-    'https://parrote.s3.amazonaws.com/uploads#{uuid}-nr.ogg'
+    'https://parrote.s3.amazonaws.com/uploads/#{uuid}-nr.ogg'
 
   else
     'URL is not in an appropriate format'
-  end
-
-end
-
-
-get '/records' do
-  filename = params['file']
-  p filename
-
-  if filename =~ FILENAME_REGEX
-    send_file filename, :type => 'audio/ogg'
-  else
-    status 400
-    'Bad file name'
   end
 
 end
@@ -63,6 +49,13 @@ def reducted(uuid)
   # Create a request on rails server
   # find record with uuid
   # change record's status to Reducted
+  uri = URI.parse("http://www.loroclip.com/api/v1/records/nr")
+  params = {
+    :url => 'https://parrote.s3.amazonaws.com/uploads/#{uuid}-nr.ogg',
+    :uuid => uuid
+  }
+  res = Net::HTTP.post_form(uri, params)
+
 end
 
 def upload(filename)
